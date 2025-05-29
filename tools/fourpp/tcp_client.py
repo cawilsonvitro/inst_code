59fff16377f92a551d9b04b81d38f49b7f48b163
cawilsonvitro <cawilson@vitro.com>
2025-05-29 12:06:01 -0400

    some tcp changes

--- tools/fourpp/tcp_client.py
+++ tools/fourpp/tcp_client.py
@@ -1,39 +1,64 @@
+# client side:
+
 import socket
-import json
-
-host = None
-
-with open('config.json', 'r') as file:
-    ips = json.load(file)["Tool_ip"]
-    for key in list(ips.keys()):
-        if ips[key] == "Host":
-            host = key
-            
-            
-            
-print(host)
+import sys
 
 
-
-def client_program(host):
-    host = host  # as both code is running on same pc
-    port = 5000  # socket server port number
-
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
-
-    message = input(" -> ")  # take input
-
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
+SERVER = "192.168.1.1"
+PORT = 5050
+ADDR = (SERVER, PORT)
 
 
-if __name__ == '__main__':
-    client_program(host)
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    soc.connect(ADDR)
+
+except TimeoutError or ConnectionResetError:
+    print(
+        "\nCould not receive a response from the server.\n"
+        "Please check the IP and PORT numbers provided.\n"
+    )
+    sys.exit()
+
+except OSError or ConnectionRefusedError:
+    print(
+        "\nPlease check if the server is connected to the internet\n"
+        "and that the IP and PORT numbers are correct on both ends\n"
+    )
+    sys.exit()
+
+
+data = ""
+flag = 0
+
+
+while data != "Bye":
+    try:
+        if flag == 0:
+            long_msg_txt = (
+                "\nWelcome! \n\n"
+                "Please enter your message, or type:\n"
+                "NAME - to get the name of the server\n"
+                "TIME -  to get the current time and date\n"
+                "RAND - to receive a random number between 0-10\n"
+                "QUIT or Q - to close the socket\n\n"
+                "Enter your message Here: "
+            )
+            long_msg = input(long_msg_txt)
+            soc.send(long_msg.encode())
+            flag += 1
+        else:
+            msg = input("Enter your message Here: ")
+            soc.send(msg.encode())
+        data = soc.recv(1024).decode()
+    except ConnectionResetError or ConnectionAbortedError:
+
+        print("\nThe connection was forcibly closed by the remote host\n")
+        break
+    else:
+        # received message from server(1024=max bytes size)
+        print(f"\nThe server sent: {data}\n")
+
+# when data == "Bye"
+print("Closing the socket with the server")
+soc.close()  # close the connection
