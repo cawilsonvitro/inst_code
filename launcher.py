f5f25e28cb2c26cde6b28744c62b162864ee60ac
cawilsonvitro <cawilson@vitro.com>
2025-06-23 09:12:51 -0400

    .venv and launcher fixes

--- launcher.py
+++ launcher.py
@@ -2,8 +2,10 @@ import json
 import socket
 import os
 import typing
+import os
 
 
+virt_path: str = os.path.join(os.getcwd(), '.venv', 'scripts', 'python.exe')
 
 file: typing.TextIO = open('config.json', 'r')
 config:dict[str, str] = json.load(file)['Tool_ip']
@@ -17,6 +19,7 @@ ip_address = socket.gethostbyname(hostname)
 
 try:
     tool: str = config[ip_address]
+    print(tool)
 except KeyError:
     print(f"IP address {ip_address} not found in config.json.")
     tool = "" 
@@ -24,24 +27,25 @@ except KeyError:
 
 file_name: str = "main"
 
+
+
 if tool != "host" and tool != "testing":
     
     file_name += tool
-
     file_name += ".py"
-
-    file_name = f"tools//{tool}//{file}"
+    file_name = f"tools//{tool}//{file_name}"
     
 elif tool != "testing":
     file_name = f"{file_name}.py"
 
 else:
     server_ip = "127.0.0.1"
-    
 
+print(file_name)
+py = virt_path
 server_ip = list(config.keys())[0]
 if file_name != "testing":
-    os.system(f"py {file_name} {server_ip}")
+    os.system(f"{py} {file_name} {server_ip}")
 
 
 
