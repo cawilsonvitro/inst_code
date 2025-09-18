82df8b05b0b0e2c51179b2f7b5cb12eb982c4773
cawilsonvitro <cawilson@vitro.com>
2025-09-18 07:51:29 -0400

    Add time import and clean up measure method in fourpp_dummy

--- tools/fourpp/fourpp_dummy.py
+++ tools/fourpp/fourpp_dummy.py
@@ -1,7 +1,7 @@
 import pyvisa
 import numpy as np
 import random
-
+import time as t
 class siglent():
 
     def __init__(self,connection_string, **kwargs):
@@ -26,7 +26,7 @@ class siglent():
     def measure(self):
         self.values = [7,8,9,10]
 
-        
+        t.sleep(5)
 
     def quit(self):
         self.status = False
