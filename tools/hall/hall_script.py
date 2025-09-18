9832d85f31c8e39b2739afd796ad8dbbbd792a3a
cawilsonvitro <cawilson@vitro.com>
2025-09-18 11:59:11 -0400

    Enhance GUI functionality in silent_hall: add toggle and update methods for description and ID windows, improve dropdown handling, and refine layout labels.

--- tools/hall/hall_script.py
+++ tools/hall/hall_script.py
@@ -60,7 +60,55 @@ class silent_hall:
         self.buildGUI(self.root)
         
         self.root.mainloop()
-    #region gui 
+    
+    #region GUI 
+    
+       
+        
+    def toggle_desc(self):
+        self.logger.info("Toggling Description Window")
+        state = self.desc_window.state()
+        if state == "normal": self.desc_window.withdraw()
+        if state == "withdrawn": self.desc_window.deiconify()
+
+    
+    def get_desc(self, event) ->None:
+        #closes out window and gets info
+        self.description = TextBox.instances["desc"].get("1.0","end-1c")
+        TextBox.instances["desc"].delete("1.0","end-1c")
+        self.toggle_desc()
+        
+        self.wait.set(False)
+    
+    def get_pos(self,event) -> None:        
+        self.logger.debug("Getting pos")
+        self.position = "" 
+        self.position = dropdown.instances["position"].get()
+        self.logger.debug(f"{self.position} selected")
+        
+    def update(self) -> None:
+        self.logger.info("Updating sample dropdown")
+        self.tcp.soc.send("UPDATE".encode())
+        resp:str = self.tcp.soc.recv(1024).decode()
+        print(resp)
+        if resp != "None":
+            dropdown.instances["samples"].configure(values=resp.split(","))
+        else:
+            dropdown.instances["samples"].configure(values=[])
+    
+    def toggle_id(self):
+        self.logger.info("Toggling ID Window")
+        state = self.id_window.state()
+        if state == "normal":self.id_window.withdraw()
+        if state == "withdrawn":self.id_window.deiconify()
+        
+    def get_id(self, event) -> None:
+        self.logger.debug("Getting ID")
+        self.id = TextBox.instances["id"].get("1.0","end-1c")
+        print(self.id)
+        TextBox.instances["id"].delete("1.0","end-1c")
+        self.toggle_id()
+    
     def buildGUI(self, root):
         """_summary_ builds gui
 
@@ -75,8 +123,47 @@ class silent_hall:
             width = 28,
             postcommand=self.update
         ).place(x = 0, y = 60)
-        dropdown.instances["samples"].bind("<<ComboboxSelected>>",self.callback)
-    
+        # dropdown.instances["samples"].bind("<<ComboboxSelected>>",self.callback) dont need this
+
+        dropdown(
+            "position",
+            root,
+            values = ["LT", "LC", "LL", "CT", "CC", "CL", "RT", "RC", "RL"],
+            width = 5,
+        ).place(x = 0, y = 120)
+        dropdown.instances["position"].bind('<<ComboboxSelected>>', self.get_pos)
+
+        Label(
+            "positions",
+            root,
+            text = "position:",
+            anchor=tk.W,           
+            height=1,              
+            width=30,              
+            bd=1,                  
+            font=("Arial", 10), 
+            cursor="hand2",   
+            fg="black",                           
+            justify = tk.LEFT,  
+            wraplength=100   
+            ).place(x = 0, y = 100, width = 80,height = 20)
+        
+        Label(
+            "Samples",
+            root,
+            text = "sample ID:",
+            anchor=tk.W,           
+            height=1,              
+            width=30,              
+            bd=1,                  
+            font=("Arial", 10), 
+            cursor="hand2",   
+            fg="black",                           
+            justify = tk.LEFT,  
+            wraplength=100   
+            ).place(x = 0, y = 40, width = 80,height = 20)
+        
+    #endregion
     
     def callback(self, eventObject):
         self.endApp(None)
@@ -105,7 +192,7 @@ class silent_hall:
         self.quit = True
         self.sample_num = dropdown.instances["samples"].get()
         if self.sample_num == "":
-                print("PLease enter a sample number")
+                print("Please enter a sample number")
         else:
             self.root.quit()
         
