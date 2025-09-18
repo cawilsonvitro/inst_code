8677fc94ac434855b473b08d112f67660ba17bf3
cawilsonvitro <cawilson@vitro.com>
2025-09-18 12:21:37 -0400

    Refactor logging setup and enhance GUI in silent_hall: add description and ID windows, improve dropdown handling, and implement state management for user input.

--- tools/hall/hall_script.py
+++ tools/hall/hall_script.py
@@ -6,11 +6,27 @@ from instutil import inst_util as iu
 import sys
 from gui_package_cawilvitro import *
 import tkinter as tk
-#endregion 
-
-
+from functools import partial
 import sys
 import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from datetime import datetime as dt
+#endregion 
+
+#region logging
+date = dt.now().strftime("%m-%d-%Y, Hour %H Min %M Sec %S")
+
+logging.basicConfig(
+    level=logging.DEBUG, # Set a global logging level
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        # logging.StreamHandler(), # Log to console
+        TimedRotatingFileHandler(f'tools\\hall\\logs\\{date}.log', when = "D", backupCount= 5)
+    ]
+)
+
+#endregion 
 
 cwd = os.getcwd()
 
@@ -61,7 +77,6 @@ class silent_hall:
         
         self.root.mainloop()
     
-    #region GUI 
     
        
         
@@ -115,7 +130,16 @@ class silent_hall:
         Args:
             root (_type_): tk root
         """
+        
+                #to track our var
+        self.wait:tk.BooleanVar = tk.BooleanVar()
+        
+        self.wait.set(False)
+        
+        
         dropdown.remove(None)
+        Label.remove(None)
+        
         dropdown(
             "samples",
             root,
@@ -163,8 +187,39 @@ class silent_hall:
             wraplength=100   
             ).place(x = 0, y = 40, width = 80,height = 20)
         
-    #endregion
-    
+        #description window
+        TextBox.remove(None) #here so we only have to call this once might havet to switch
+        self.desc_window = tk.Toplevel(self.root)
+        self.desc_window.geometry("300x800")
+        self.desc_window.title("Sample Description")
+        self.desc_window.bind('<Escape>', self.get_desc)
+        self.desc_window.protocol("WM_DELETE_WINDOW", partial(self.get_desc, None))
+        TextBox("desc", self.desc_window, height = 20, width = 32).place(x = 10, y = 50)
+        self.desc_window.withdraw()
+        
+        #operator id window
+        self.id_window = tk.Toplevel(self.root)
+        self.id_window.geometry("300x300")
+        self.id_window.title("Operator ID")
+        self.id_window.bind('<Escape>', self.get_id)
+        self.id_window.protocol("WM_DELETE_WINDOW", partial(self.get_id, None))
+        TextBox("id", self.id_window, height = 2, width = 30).place(x = 10, y = 50)
+        Label(
+            "Operator_ID",
+            self.id_window,
+            text = "Operator ID:",
+            anchor=tk.W,           
+            height=1,              
+            width=30,              
+            bd=1,                  
+            font=("Arial", 10), 
+            cursor="hand2",   
+            fg="black",                           
+            justify = tk.LEFT,  
+            wraplength=100   
+            ).place(x = 0, y = 30, width = 80,height = 20)
+        self.id_window.withdraw()
+        
     def callback(self, eventObject):
         self.endApp(None)
 
@@ -199,7 +254,7 @@ class silent_hall:
             
     #endregion
     
-    
+#region state system  
     def state_sys(self):
         """
             uses a txt file to keep track of number of files before and
@@ -255,21 +310,23 @@ class silent_hall:
                 self.client.connect()
                 self.client.id()
                 self.starApp()
+                
+                
                 for file in new_files:
                     # print(file)
                     path = os.path.join("data", file)
-                    self.client.soc.send("MEAS".encode())
-                    resp = self.client.soc.recv(1024).decode()
-                    self.client.soc.send(self.sample_num.encode())
-                    resp = self.client.soc.recv(1024).decode()
-                    _,data = iu.parse(path)
-                    data_str = (",").join(data)
+                    self.wait.set(True)
+                    self.id_window.title(f"Operator ID for {file}")
+                    self.toggle_id()
+                    self.root.wait_variable(self.wait)
                     
-                    self.client.soc.send(data_str.encode())
-                    
+                    self.desc_window.title(f"Description for {file}")
+                    self.wait.set(True)
+                    self.toggle_desc()
+                    self.root.wait_variable(self.wait)
                     
                 self.client.disconnect()
-                raise Exception
+                raise Exception #this is to prevent new file from being marked as read, please comment to run normally
                 with open(self.tracker, "r") as f:lines=f.readlines()
                 
                 lines[1] = "True"
@@ -289,6 +346,73 @@ class silent_hall:
         else:
             print("No new files detected")
 
+    def tcp_protocol(self, path):
+        
+        self.logger.info("Starting TCP protocol")
+        
+        self.logger.debug("Sending META command to server")
+        
+        self.tcp.soc.send("META".encode())
+        resp = self.tcp.soc.recv(1024).decode()
+        
+        self.logger.debug(f"Received response: {resp}")
+        self.logger.debug("Sending sample number to server")
+        
+        self.tcp.soc.send(str(self.sample_num).encode())
+        resp = self.tcp.soc.recv(1024).decode()
+        
+        self.logger.debug(f"Received response: {resp}")
+        self.logger.debug("Sending position to server")
+        if self.position == "": self.position = "None"
+        
+        self.tcp.soc.send(self.position.encode())
+        self.description = self.tcp.soc.recv(1024).decode()
+        
+        self.logger.debug(f"got {self.description} from server")
+        self.logger.debug("Sending description request to server")
+
+        TextBox.instances["desc"].insert("1.0", self.description)
+        self.logger.debug(f"Server sent sample description {self.description}, launched description editor")
+        self.process_display.set("Please enter sample, if no description needed enter none")
+        self.wait.set(True)
+        self.toggle_id()
+        self.root.wait_variable(self.wait)
+            
+        
+        self.wait.set(True)
+        self.toggle_desc()
+        self.root.wait_variable(self.wait)
+        self.logger.debug(f"user set description to {self.description}")
+
+        self.logger.info("Starting TCP MEAS protocol")
+
+        self.logger.debug("Sending MEAS command")
+        
+        self.tcp.soc.send("MEAS".encode())
+        
+        resp = self.tcp.soc.recv(1024).decode()
+        self.logger.debug(f"got {resp} from Server")
+        self.logger.debug(f"sending sample id {self.sample_num}")
+        
+        self.tcp.soc.send(self.sample_num.encode())
+        resp = self.tcp.soc.recv(1024).decode()
+        
+        self.logger.debug(f"Received response: {resp}")
+        
+        self.tcp.soc.send(self.description.encode())
+        resp = self.tcp.soc.recv(1024).decode()
+        
+        self.logger.debug(f"Received response: {resp}")
+        
+        self.tcp.soc.send(str(self.value).encode())
+        resp = self.tcp.soc.recv(1024).decode()
+        
+        self.logger.debug(f"Received response: {resp}")
+        if resp != "data received":
+           self.logger.error(f"unexpected response from server {resp}")
+        else:
+            self.logger.info("TCP protocol complete")
+    #endregion
 
 
 if __name__ == "__main__":
