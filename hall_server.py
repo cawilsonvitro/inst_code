a89f6b0297bf876b3ec34f45d134f2d0b9de2c43
cawilsonvitro <cawilson@vitro.com>
2025-09-22 09:35:12 -0400

    Add hall server implementation and update config for hall testing

---
+++ hall_server.py
@@ -0,0 +1,10 @@
+#region imports
+from instutil import inst_util as iu
+import socket
+#endregion
+
+
+test_server = iu.tcp_multiserver("config.json","10.40.0.16",5051,None)
+test_server.SQL_startup()
+test_server.server()
+
